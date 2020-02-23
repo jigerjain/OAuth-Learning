4532fee891351a85a7971d3f415c9d2c0896e32a
Jiger Jain <jiger.jain@citrix.com>
2020-02-23 18:54:38 -0500

    Oauth setup with api calls

--- index.php
+++ index.php
@@ -1,28 +1,7 @@
 <?php
 
-function apiRequest($url, $post=FALSE, $headers=array()){
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-    if($post)
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
-    
-    $headers = [
-        'Accept: application/vnd.github.v3+json, application/json',
-        'User-Agent: https://exmaple-app.com'
-    ];
-
-    if(isset($_SESSION['access_token']))
-        $headers[] = 'Authorization: Bearer '.$_SESSION['access_token'];
-
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-    $response = curl_exec($ch);
-    return json_decode($response, true);
-}
-
 // Fill out values from Github
-$githubClientID = '8771161b48146cb81269';
+$githubClientID = '';
 $githubClientSecret = '';
 
 // This is the url we will send the user to first get their authorization
@@ -32,7 +11,8 @@ $authorizeURL = "https://github.com/login/oauth/authorize";
 $token_URL = "https://github.com/login/oauth/access_token";
 
 // This is the Github base URL for API Requests
-$apiURLBase = "https://api.github.com";
+$apiURLBase = "https://api.github.com/";
+
 
 // This URL for this script, used as the redirect URL
 $baseURL = "http://".$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF'];
@@ -40,42 +20,25 @@ $baseURL = "http://".$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF'];
 //Start a session so we have a place to store things between redirects 
 session_start();
 
-if(!isset($_GET['action'])){
-    if(!empty($_SESSION['access_token'])){
-        echo '<h3>logged in </h3>';
-        echo '<p><a href="?action=repos"> View Repos </a> </p>';
-        }
-        
-    else{
-        echo '<h3> Not logged in </h3>';
-        echo '<p><a href="?action=login"> Log In </a> </p>';
-    }
-    die();
-}
-
-
-
 // Start the login process by sending the user to the Github Login page
-
 if(isset($_GET['action']) && $_GET['action']=='login'){
     unset($_SESSION['access_token']);
+
+    // Generate a random hash:
+    $_SESSION['state'] = bin2hex(random_bytes(16));
+
+    $params = array(
+        'response_type' => 'code',
+        'client_id' => $githubClientID,
+        'redirect_uri' => $baseURL,
+        'scope' => 'user public_group',
+        'state' => $_SESSION['state']
+    );
+
+    // Redirect the user to Github's auth page:
+    header('location: '.$authorizeURL.'?'.http_build_query($params));
+    die();
 }
-
-// Generate a random hash:
-$_SESSION['state'] = bin2hex(random_bytes(16));
-
-$params = array(
-    'response_type' => 'code',
-    'client_id' => $githubClientID,
-    'redirect_uri' => $baseURL,
-    'scope' => 'user public_group',
-    'state' => $_SESSION['state']
-);
-
-// Redirect the user to Github's auth page:
-
-header('location: '.$authorizeURL.'?'.http_build_query($params));
-die();
 
 if(isset($_GET['code'])){
     // Verify the state of the return parameter
@@ -85,17 +48,67 @@ if(isset($_GET['code'])){
         die();
     }
 
-// Exchange the auth code for access token
-$token = apiRequest($token_URL, array(
-    'grant_type' => 'authorization_code',
-    'client_id' => $githubClientID,
-    'client_secret' => $githubClientSecret,
-    'redirect_uri' => $baseURL,
-    'code' => $_GET['code']
-));
+    //Exchange the auth code for access token
+    $token = apiRequest($token_URL, array(
+        'grant_type' => 'authorization_code',
+        'client_id' => $githubClientID,
+        'client_secret' => $githubClientSecret,
+        'redirect_uri' => $baseURL,
+        'code' => $_GET['code']
+    ));
+    $_SESSION['access_token'] = $token['access_token'];
 
-$_SESSION['access_token'] = $token['access_token'];
-header('location: '.$baseURL);
-die();
+    header('location: '.$baseURL);
+    die();
+}
+
+if(isset($_GET['action']) && $_GET['action']=='repos'){
+    // Find all repos created by the authenticated user
+    $repos = apiRequest($apiURLBase.'user/repos?'.http_build_query(['sort'=>'created', 'direction' => 'desc']));
+
+    echo '<ul>';
+    foreach($repos as $repo){
+        echo '<li> <a href="' . $repo['html_url']. '"> ' .$repo['name'] . '</a></li>';
+    }
+
+    echo '</ul>';
+    
+}
+
+if(!isset($_GET['action'])){
+    if(!empty($_SESSION['access_token'])){
+        echo '<h3>logged in </h3>';
+        echo '<p><a href="?action=repos"> View Repos </a> </p>';
+        }  
+    else{
+        echo '<h3> Not logged in </h3>';
+        echo '<p><a href="?action=login"> Log In </a> </p>';
+    }
+    die();
+}
+
+
+function apiRequest($url, $post=FALSE, $headers=array()){
+   
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+    if($post)
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+    
+    $headers = [
+        'Accept: application/vnd.github.v3+json, application/json',
+        'User-Agent: http://localhost:80'
+    ];
+
+    if(isset($_SESSION['access_token']))
+        $headers[] = 'Authorization: Bearer '.$_SESSION['access_token'];
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+
+    $response = curl_exec($ch);
+
+    return json_decode($response, true);
 }
 
